e80228ad5cfc706a3a9ec7692f512c08c1fe5059
Oleh Medelyan <olezhyk88@gmail.com>
2017-05-19 20:32:48 +0300

    Add installer

--- Payroll Management System/Payroll Management System/Entities/SalaryInfo.cs
+++ Payroll Management System/Payroll Management System/Entities/SalaryInfo.cs
@@ -1,9 +1,4 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
-using System.Collections.ObjectModel;
+﻿using System.Collections.ObjectModel;
 using System.ComponentModel.DataAnnotations;
 using System.ComponentModel.DataAnnotations.Schema;
 
@@ -20,5 +15,8 @@ namespace Payroll_Management_System.Entities
 
         [Required]
         public double Tax { get; set; }
+
+
+        public virtual ObservableCollection<Employee> Employees { get; private set; }
     }
 }
